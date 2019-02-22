81f7deba76d1e8439170081001e6bbb667b9a0fb
b93_100007542592_xyzgy <b93_100007542592_xyzgy@git.cloud.tencent.com>
2019-02-22 18:25:10 +0800

    proxy代理配置

--- vue.config.js
+++ vue.config.js
@@ -29,5 +29,17 @@ module.exports = {
 				]
 			}
 		}
-	}
+	},
+	devServer: {
+        proxy: {
+            '/apis': {
+                target: 'https://movie.douban.com/',  // target host
+                ws: true,  // proxy websockets 
+                changeOrigin: true,  // needed for virtual hosted sites
+                pathRewrite: {
+                    '^/apis': ''  // rewrite path
+                }
+            },
+        }
+    }
 }
